10f490cee2acf5e65c2c6db88d7cc9e159db8e8c
gaurab <gaurabbehera220@gmail.com>
2022-10-18 21:10:38 +0530

    Mern_stack_frontend_ui

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+const API_URL = '/api/users/'
+
+// Register User
+const register = async (userData) => {
+    const response = await axios.post(API_URL, userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+// Login User
+const login = async (userData) => {
+    // It will hit the backend login of the user
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+
+// Logout User
+const logout = () => {
+    localStorage.removeItem('user')
+    
+}
+
+const authService = {
+    register,
+    login,
+    logout,
+}
+
+export default authService
